c8ef39204d7167204cd2cd656ed6ff38cdec97e1
Chirag Pandey <chiragpandey15@gmail.com>
2024-04-30 18:00:53 -0700

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -165,100 +165,100 @@ app.post('/publishText',async(req,res)=>{
   res.json({"stauts":"ok"});
 });
 
-app.post('/publishTextImage',async (req, res) => {
+// app.post('/publishTextImage',async (req, res) => {
   
-  const imageData = req.body.image;
-  const base64Data = imageData.replace(/^data:image\/\w+;base64,/, '');
-  const image = Buffer.from(base64Data, 'base64');
-  
-
-  // Getting access Token of linkedin from clerk
-  const token = await clerkClient.users.getUserOauthAccessToken(req.body.userID, provider);
-  
-  // Getting userID from linkedin, required in future api call
-  var response  = await fetch ('https://api.linkedin.com/v2/userinfo',{
-    headers: {
-      'Authorization': `Bearer ${token[0].token}`,
-    },
-  });
-
-  const data = await response.json();
-
+//   const imageData = req.body.image;
+//   const base64Data = imageData.replace(/^data:image\/\w+;base64,/, '');
+//   const image = Buffer.from(base64Data, 'base64');
   
 
-  // registering image
-  response = await fetch('https://api.linkedin.com/v2/assets?action=registerUpload', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${token[0].token}`,
-      'Content-Type': 'application/json',
-      'X-Restli-Protocol-Version': '2.0.0'
-    },
-    body: JSON.stringify(
-      {
-        "registerUploadRequest": {
-            "recipes": [
-                "urn:li:digitalmediaRecipe:feedshare-image"
-            ],
-            "owner": `urn:li:person:${data.sub}`,
-            "serviceRelationships": [
-                {
-                    "relationshipType": "OWNER",
-                    "identifier": "urn:li:userGeneratedContent"
-                }
-            ]
-        }
-    }
-    )
-  });
+//   // Getting access Token of linkedin from clerk
+//   const token = await clerkClient.users.getUserOauthAccessToken(req.body.userID, provider);
+  
+//   // Getting userID from linkedin, required in future api call
+//   var response  = await fetch ('https://api.linkedin.com/v2/userinfo',{
+//     headers: {
+//       'Authorization': `Bearer ${token[0].token}`,
+//     },
+//   });
 
-  const uploadRequest = await response.json();
+//   const data = await response.json();
+
   
 
-  // Uploading image
-  await fetch(`${uploadRequest.value.uploadMechanism['com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest'].uploadUrl}`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${token[0].token}`,
-      'Content-Type': 'image/png'
-    },
-    body: image
-  });
+//   // registering image
+//   response = await fetch('https://api.linkedin.com/v2/assets?action=registerUpload', {
+//     method: 'POST',
+//     headers: {
+//       'Authorization': `Bearer ${token[0].token}`,
+//       'Content-Type': 'application/json',
+//       'X-Restli-Protocol-Version': '2.0.0'
+//     },
+//     body: JSON.stringify(
+//       {
+//         "registerUploadRequest": {
+//             "recipes": [
+//                 "urn:li:digitalmediaRecipe:feedshare-image"
+//             ],
+//             "owner": `urn:li:person:${data.sub}`,
+//             "serviceRelationships": [
+//                 {
+//                     "relationshipType": "OWNER",
+//                     "identifier": "urn:li:userGeneratedContent"
+//                 }
+//             ]
+//         }
+//     }
+//     )
+//   });
+
+//   const uploadRequest = await response.json();
   
-  // Uploading content 
-  await fetch('https://api.linkedin.com/v2/ugcPosts', {
-  method: 'POST',
-  headers: {
-    'Authorization': `Bearer ${token[0].token}`,
-    'Content-Type': 'application/json',
-    'X-Restli-Protocol-Version': '2.0.0'
-  },
-  body: JSON.stringify({
-    "author": `urn:li:person:${data.sub}`,
-    "lifecycleState": "PUBLISHED",
-    "specificContent": {
-      "com.linkedin.ugc.ShareContent": {
-        "shareCommentary": {
-          "text": body.text
-        },
-        "shareMediaCategory": "IMAGE",
-        "media": [
-            {
-                "status": "READY",
-                "media": `${uploadRequest.value.asset}`,
+
+//   // Uploading image
+//   await fetch(`${uploadRequest.value.uploadMechanism['com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest'].uploadUrl}`, {
+//     method: 'POST',
+//     headers: {
+//       'Authorization': `Bearer ${token[0].token}`,
+//       'Content-Type': 'image/png'
+//     },
+//     body: image
+//   });
+  
+//   // Uploading content 
+//   await fetch('https://api.linkedin.com/v2/ugcPosts', {
+//   method: 'POST',
+//   headers: {
+//     'Authorization': `Bearer ${token[0].token}`,
+//     'Content-Type': 'application/json',
+//     'X-Restli-Protocol-Version': '2.0.0'
+//   },
+//   body: JSON.stringify({
+//     "author": `urn:li:person:${data.sub}`,
+//     "lifecycleState": "PUBLISHED",
+//     "specificContent": {
+//       "com.linkedin.ugc.ShareContent": {
+//         "shareCommentary": {
+//           "text": body.text
+//         },
+//         "shareMediaCategory": "IMAGE",
+//         "media": [
+//             {
+//                 "status": "READY",
+//                 "media": `${uploadRequest.value.asset}`,
                 
-            }
-        ]
-      }
-    },
-    "visibility": {
-      "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
-    }
-  })
-});
-// res.json({"stauts":"ok"});
+//             }
+//         ]
+//       }
+//     },
+//     "visibility": {
+//       "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+//     }
+//   })
+// });
+// // res.json({"stauts":"ok"});
 
-});
+// });
 
 
 
